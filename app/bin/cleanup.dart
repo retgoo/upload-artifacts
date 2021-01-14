6d5fc8581fa3587ee3e61c55be0f7bc0f61487d3
Eka Setiawan Saputra <mr.poetra22@gmail.com>
2021-01-14 21:56:41 +0700

    update

---
+++ app/bin/cleanup.dart
@@ -0,0 +1,6 @@
+import 'package:github_actions_toolkit/github_actions_toolkit.dart' as gaction;
+
+void main(List<String> arguments) async {
+  final logger = gaction.log;
+  logger.info("finished");
+}
---
+++ app/bin/main.dart
@@ -0,0 +1,57 @@
+import 'dart:io';
+
+import 'package:archive/archive_io.dart';
+import 'package:dio/dio.dart';
+import 'package:github_actions_toolkit/github_actions_toolkit.dart' as gaction;
+
+void main(List<String> args) async {
+  exitCode = 0;
+
+  final logger = gaction.log;
+
+  final path = gaction.Input('path', isRequired: true, canBeEmpty: false);
+  logger.info('path: ${path.value}');
+
+  final zip = gaction.Input('zip', isRequired: false);
+  logger.info('zip: ${zip.value}');
+
+  final root = gaction.Input('root', isRequired: false);
+  logger.info('is root folder: ${root.value}');
+
+  final output = gaction.Input('output', isRequired: false);
+  logger.info('zip file name: ${output.value}');
+
+  final id = gaction.Input('id', isRequired: true, canBeEmpty: false);
+  logger.info('id: ${id.value}');
+
+  File data;
+  if (zip.value == 'true') {
+    print("Zipping data");
+    final encoder = ZipFileEncoder();
+    encoder.create(output.value);
+    encoder.addDirectory(Directory(path.value),
+        includeDirName: root.value == 'true');
+    encoder.close();
+
+    data = File(output.value);
+  } else {
+    data = File(path.value);
+  }
+
+  final dio = Dio(
+    BaseOptions(baseUrl: "https://api.storage.retgoo.id/share/"),
+  );
+
+  final response = await dio.post(
+    id.value,
+    data: FormData.fromMap(
+      {
+        "file": await MultipartFile.fromFile(data.absolute.path),
+      },
+    ),
+  );
+
+  if (response.statusCode == 200) {
+    logger.info("artifact uploaded");
+  }
+}
